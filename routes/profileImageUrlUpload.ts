69fb1a1978b3ff889169d380fb6fc2b486a7cc09
ramicodermahfoud <rami.mahfoud@postcode-lotterie.de>
2025-12-03 14:44:00 +0100

    solve H

--- routes/profileImageUrlUpload.ts
+++ routes/profileImageUrlUpload.ts
@@ -6,32 +6,41 @@
 import fs = require('fs')
 import { type Request, type Response, type NextFunction } from 'express'
 import logger from '../lib/logger'
+import fetch from 'node-fetch'
 
 import { UserModel } from '../models/user'
 import * as utils from '../lib/utils'
 const security = require('../lib/insecurity')
-const request = require('request')
 
 module.exports = function profileImageUrlUpload () {
-  return (req: Request, res: Response, next: NextFunction) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
     if (req.body.imageUrl !== undefined) {
       const url = req.body.imageUrl
       if (url.match(/(.)*solve\/challenges\/server-side(.)*/) !== null) req.app.locals.abused_ssrf_bug = true
       const loggedInUser = security.authenticatedUsers.get(req.cookies.token)
       if (loggedInUser) {
-        const imageRequest = request
-          .get(url)
-          .on('error', function (err: unknown) {
-            UserModel.findByPk(loggedInUser.data.id).then(async (user: UserModel | null) => { return await user?.update({ profileImage: url }) }).catch((error: Error) => { next(error) })
-            logger.warn(`Error retrieving user profile image: ${utils.getErrorMessage(err)}; using image link directly`)
-          })
-          .on('response', function (res: Response) {
-            if (res.statusCode === 200) {
-              const ext = ['jpg', 'jpeg', 'png', 'svg', 'gif'].includes(url.split('.').slice(-1)[0].toLowerCase()) ? url.split('.').slice(-1)[0].toLowerCase() : 'jpg'
-              imageRequest.pipe(fs.createWriteStream(`frontend/dist/frontend/assets/public/images/uploads/${loggedInUser.data.id}.${ext}`))
-              UserModel.findByPk(loggedInUser.data.id).then(async (user: UserModel | null) => { return await user?.update({ profileImage: `/assets/public/images/uploads/${loggedInUser.data.id}.${ext}` }) }).catch((error: Error) => { next(error) })
-            } else UserModel.findByPk(loggedInUser.data.id).then(async (user: UserModel | null) => { return await user?.update({ profileImage: url }) }).catch((error: Error) => { next(error) })
-          })
+        try {
+          const imageResponse = await fetch(url)
+          if (imageResponse.ok) {
+            const ext = ['jpg', 'jpeg', 'png', 'svg', 'gif'].includes(url.split('.').slice(-1)[0].toLowerCase()) ? url.split('.').slice(-1)[0].toLowerCase() : 'jpg'
+            const filePath = `frontend/dist/frontend/assets/public/images/uploads/${loggedInUser.data.id}.${ext}`
+            const fileStream = fs.createWriteStream(filePath)
+            await new Promise((resolve, reject) => {
+              imageResponse.body?.pipe(fileStream)
+              imageResponse.body?.on('error', reject)
+              fileStream.on('finish', resolve)
+            })
+            const user = await UserModel.findByPk(loggedInUser.data.id)
+            await user?.update({ profileImage: `/assets/public/images/uploads/${loggedInUser.data.id}.${ext}` })
+          } else {
+            const user = await UserModel.findByPk(loggedInUser.data.id)
+            await user?.update({ profileImage: url })
+          }
+        } catch (err: unknown) {
+          const user = await UserModel.findByPk(loggedInUser.data.id)
+          await user?.update({ profileImage: url }).catch((error: Error) => { next(error) })
+          logger.warn(`Error retrieving user profile image: ${utils.getErrorMessage(err)}; using image link directly`)
+        }
       } else {
         next(new Error('Blocked illegal activity by ' + req.socket.remoteAddress))
       }
