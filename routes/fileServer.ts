33a84ee37114a3d1c2e3a8333446c567d1440dbc
ramicodermahfoud <rami.mahfoud@postcode-lotterie.de>
2025-12-03 15:39:12 +0100

    ai fixes

--- routes/fileServer.ts
+++ routes/fileServer.ts
@@ -30,7 +30,7 @@ module.exports = function servePublicFiles () {
       challengeUtils.solveIf(challenges.directoryListingChallenge, () => { return file.toLowerCase() === 'acquisitions.md' })
       verifySuccessfulPoisonNullByteExploit(file)
 
-      res.sendFile(path.resolve('ftp/', file))
+      res.sendFile(path.resolve('ftp/', path.basename(file)))
     } else {
       res.status(403)
       next(new Error('Only .md and .pdf files are allowed!'))
